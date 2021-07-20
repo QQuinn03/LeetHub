633ddb3e448dc4c7d6a17b21f205b774dc21cbc7
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-07-20 15:03:26 -0400

    Time: 44 ms (42.20%), Space: 14.4 MB (71.35%) - LeetHub

---
+++ word-break/word-break.py
@@ -0,0 +1,10 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp=[False for i in range(len(s)+1)]
+        dp[0]= True
+        for i in range(len(dp)):
+            if dp[i]==True:
+                for j in range(i+1,len(dp)):
+                    if s[i:j] in wordDict:
+                        dp[j]=True
+        return dp[-1]                

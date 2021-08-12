8d15398ab0af69ce246019397662c862e4b78419
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-08-12 11:50:49 -0400

    Time: 55 ms (5.64%), Space: 14.3 MB (73.74%) - LeetHub

--- implement-stack-using-queues/implement-stack-using-queues.py
+++ implement-stack-using-queues/implement-stack-using-queues.py
@@ -1,28 +1,31 @@
+from collections import deque
 class MyStack:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.stack=[]
+        self.stack=deque([])
 
     def push(self, x: int) -> None:
         """
         Push element x onto stack.
         """
-        self.stack.append(x)
+        tmp = deque([x])
+        tmp.extend(self.stack)
+        self.stack=tmp
 
     def pop(self) -> int:
         """
         Removes the element on top of the stack and returns that element.
         """
-        return self.stack.pop(-1)
+        return self.stack.popleft()
 
     def top(self) -> int:
         """
         Get the top element.
         """
-        return self.stack[-1]
+        return self.stack[0]
 
     def empty(self) -> bool:
         """

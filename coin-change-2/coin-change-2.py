6ef350148b3fb8bda92795498947360335bbab19
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-08-31 22:30:37 -0400

    Time: 340 ms (43.77%), Space: 14.2 MB (95.57%) - LeetHub

--- coin-change-2/coin-change-2.py
+++ coin-change-2/coin-change-2.py
@@ -4,12 +4,9 @@ class Solution:
         dp[0]=1
         
         for i in coins:
-            for j in range(len(dp)):
+            for j in range(1,len(dp)):
                 if j==i:
                     dp[j]+=1
                 if j>i:
                     dp[j]+=dp[j-i]
-        if dp[-1]==0:
-            return 0
-
-        return dp[-1]
+        return dp[-1]            

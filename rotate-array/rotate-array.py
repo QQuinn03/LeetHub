01321efe95e1db648cf9f5c641dbbb01b6360126
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-09-14 21:12:05 -0400

    Time: 168 ms (99.64%), Space: 24.9 MB (65.13%) - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -1,23 +1,14 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+class Solution(object):
+    def rotate(self, nums, k):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        count =start = 0
-        k %=n
+        k %=len(nums)
+        length = len(nums)
+        nums[:] = nums[length-k:]+nums[:length-k]
+
+
         
-        while count<n:
-            cur,pre = start,nums[start]
-            while True:
-                nxt_idx = (cur+k)%n
-                nums[nxt_idx],pre = pre,nums[nxt_idx]
-                cur = nxt_idx
-                count+=1
-                
-                if start==cur:
-                    print(start,cur)
-                    break
-            start+=1
-  
         

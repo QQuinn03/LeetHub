dc4a6139ea0aaf71d7e32fbeb5a9708128276754
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-09-27 14:55:21 -0400

    Time: 292 ms (38.83%), Space: 28.8 MB (32.68%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,23 +1,33 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid or len(grid)==0:
+            return 0
         row = len(grid)
         col = len(grid[0])
-        
-        res = 0
+   
+        count=0
         for i in range(row):
             for j in range(col):
                 if grid[i][j]=="1":
-                    res+=1
+                    count+=1
                     self.helper(grid,i,j,row,col)
-        return res
-    
+        return count
     
     def helper(self,grid,i,j,row,col):
-        if i<0 or j <0 or i>=row or j >=col or grid[i][j]=="0":
-            return 
+        if i <0 or i>=row or j<0 or j>=col or grid[i][j]=="0":
+            return
         grid[i][j]="0"
+        
         self.helper(grid,i+1,j,row,col)
         self.helper(grid,i-1,j,row,col)
         self.helper(grid,i,j+1,row,col)
         self.helper(grid,i,j-1,row,col)
+                    
+
+
+        
         

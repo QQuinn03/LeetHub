0f94153c1dd10af2459128db866f5c7ff45e2d65
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-09-28 13:12:10 -0400

    Time: 60 ms (76.27%), Space: 16.6 MB (97.68%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.py
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,24 +1,24 @@
-class Solution:
-    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        start = []
-        end =[]
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        start=[]
+        end = []
         for i in intervals:
             start.append(i[0])
             end.append(i[1])
+        low = 0
+        high=0
+        res=0
         start.sort()
         end.sort()
-        
-        res = 0
-        idx = 0
-        high = 0
-        while idx<len(start):
-            
-            while idx<=len(start)-1 and start[idx]<end[high]:
+        while low<len(start):
+            while low+1<=len(start) and start[low]<end[high]:
+                low+=1
                 res+=1
-                idx+=1
             else:
                 high+=1
-            idx+=1
+            low+=1
         return res    
-                
-            

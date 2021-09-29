884039581c6ac902db76123b94470f540a171edc
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-09-28 21:32:15 -0400

    Time: 76 ms (97.13%), Space: 18.2 MB (32.45%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,17 +1,19 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
         dic = {}
         for i in strs:
             temp = "".join(sorted(i))
-     
             if temp in dic:
                 dic[temp].append(i)
             else:
                 dic[temp]=[i]
-     
-        res=[]
-        for i,val in dic.items():
+        
+        res = []
+        for key,val in dic.items():
             res.append(val)
-        return res    
-                
-            
+        
+        return res

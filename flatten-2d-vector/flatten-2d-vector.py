902377dff8b5c5837b3ea58e6ae736c9cb3ad52a
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-09-29 13:09:25 -0400

    Time: 80 ms (75.14%), Space: 19.4 MB (36.28%) - LeetHub

--- flatten-2d-vector/flatten-2d-vector.py
+++ flatten-2d-vector/flatten-2d-vector.py
@@ -1,26 +1,32 @@
 class Vector2D:
 
-    def __init__(self, v: List[List[int]]):
-        self.v = v
+    def __init__(self, vec: List[List[int]]):
+        self.v = vec
+        self.row=0
         self.col = 0
-        self.row = 0
         
 
     def next(self) -> int:
-        self.hasNext()
-        val = self.v[self.row][self.col]
+        if self.hasNext()==False:
+            return
+        val=self.v[self.row][self.col]
         self.col+=1
         return val
+        
+    
+        
 
     def hasNext(self) -> bool:
-        while self.row <len(self.v):
-            if self.col <len(self.v[self.row]):
+        while self.row<len(self.v):
+            if self.col<len(self.v[self.row]):
                 return True
             self.row+=1
-            self.col=0
-        return False
+            self.col=0 
+        return False     
+        
+
 
 # Your Vector2D object will be instantiated and called as such:
-# obj = Vector2D(v)
+# obj = Vector2D(vec)
 # param_1 = obj.next()
 # param_2 = obj.hasNext()

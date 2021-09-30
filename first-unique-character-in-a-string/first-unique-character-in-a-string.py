7de6f3edb7de244a78d08bebf7f0664e45aaf780
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-09-30 14:38:15 -0400

    Time: 128 ms (60.22%), Space: 15.5 MB (5.24%) - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,20 +1,18 @@
-class Solution(object):
-    def firstUniqChar(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
         dic = {}
         for i in range(len(s)):
             if s[i] not in dic:
-                dic[s[i]]=i
+                dic[s[i]]=[i]
             else:
-                dic[s[i]]+=len(s)
-        res = len(s)
-        
-        for i in dic:
-            if dic[i]<res:
-                res=dic[i]
-        if res== len(s):
+                dic[s[i]].append(i)
+        ans=float("inf")
+        for i,val in dic.items():
+            if len(val)==1:
+                if val[0]<ans:
+                    ans=val[0]
+        if ans==float("inf"):
             return -1
-        return res
+        return ans            
+                
+                

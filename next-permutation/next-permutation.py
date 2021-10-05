ba834f65568a51c12224a527d4d6f35e5c7cd597
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-05 14:58:34 -0400

    Time: 40 ms (82.83%), Space: 14.3 MB (18.81%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -1,26 +1,32 @@
-class Solution(object):
-    def nextPermutation(self, nums):
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        if len(nums)==1:
-            return nums
-        idx = len(nums)-2
-      
-        while idx>=0 and nums[idx]>=nums[idx+1]:
-            idx-=1
-            
-        if idx==-1:
-            return nums.reverse()
-        change = idx+1
+        #1 2 6 4 2
+        #nums[i]<nums[i+1] 2
+        #[6 4 2] cloest to 2   4
+        #14226
         
+        idx = len(nums)-2
+        
+        while idx!=-1:
+            if nums[idx]>=nums[idx+1]:
+                idx-=1
+            else:
+                break
+        if idx ==-1:
+           
+            return nums.sort()
+        print(idx)
+        change = idx+1
         while change<len(nums) and nums[change]>nums[idx]:
             change+=1
-        
+            
         change-=1
+        print(change)
         nums[idx],nums[change]=nums[change],nums[idx]
-        print(nums)
         nums[idx+1:]=sorted(nums[idx+1:])
         return nums
+        
         

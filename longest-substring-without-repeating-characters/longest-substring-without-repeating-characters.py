5ad68e7e77064f06cc7d9abd04db637cfdf943e6
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-06 12:13:07 -0400

    Time: 36 ms (96.08%), Space: 13.9 MB (48.13%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -6,24 +6,20 @@ class Solution(object):
         """
         dic = {}
         left=0
-        right = 0
+        right=0
         res=0
         
         while right<len(s):
             if s[right] not in dic:
                 dic[s[right]]=right
-                print(right,left)
-                res= max(res,right-left+1)
-                
+                res = max(res,right-left+1)
             else:
                 if dic[s[right]]>=left:
-                    left = dic[s[right]]+1
-                    print("left",left)
-                   
+                    left=dic[s[right]]+1
+                  
                 else:
                     
-                    res=max(res,right-left+1) 
-                dic[s[right]]=right   
-                   
-            right+=1        
-        return res            
+                    res=max(res,right-left+1)
+                dic[s[right]]=right    
+            right+=1
+        return res     

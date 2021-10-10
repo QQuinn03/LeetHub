649c2d866ef99e30e5f558dc88868828ee1dd075
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-10 16:43:13 -0400

    Time: 863 ms (24.17%), Space: 26.7 MB (87.43%) - LeetHub

--- palindrome-partitioning/palindrome-partitioning.py
+++ palindrome-partitioning/palindrome-partitioning.py
@@ -1,16 +1,16 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = []
-        self.dfs(s,[],res)
+        res=[]
+        self.helper(res,s,[])
         return res
     
-    def dfs(self,s,path,res):
+    def helper(self,res,s,path):
         if not s:
             res.append(path)
-        
+            return 
         for i in range(1,len(s)+1):
-            if self.check(s[:i]):
-                self.dfs(s[i:],path+[s[:i]],res)
-    
+            if self.check(s[:i])==True:
+                self.helper(res,s[i:],path+[s[:i]])
+                
     def check(self,s):
         return s==s[::-1]

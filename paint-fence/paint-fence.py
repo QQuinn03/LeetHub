ce003758b165d081fe95062d3cc29607a0b043e4
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-10 21:45:52 -0400

    Time: 40 ms (27.25%), Space: 14.1 MB (95.23%) - LeetHub

--- paint-fence/paint-fence.py
+++ paint-fence/paint-fence.py
@@ -1,19 +1,15 @@
-class Solution(object):
-    def numWays(self, n, k):
-        """
-        :type n: int
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
         if n ==1:
             return k
-        if n ==2:
+        if n==2:
             return k*k
         
-        res = [0]*(n+1)
-        res[1]=k
-        res[2]=k*k
-        
-        for i in range(3,len(res)):
+        res=[0 for i in range(n)]
+        res[0]=k
+        res[1]=k*k
+        for i in range(2,len(res)):
             res[i]=(k-1)*(res[i-1]+res[i-2])
-        return res[n]    
+        return res[-1]    
+            
+            

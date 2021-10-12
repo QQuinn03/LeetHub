99cdfb75874199ddf3526644dd166384e3e90423
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-12 15:18:11 -0400

    Time: 92 ms (63.05%), Space: 15.7 MB (44.70%) - LeetHub

--- basic-calculator-ii/basic-calculator-ii.py
+++ basic-calculator-ii/basic-calculator-ii.py
@@ -1,26 +1,30 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        res = 0
-        operand = 0
-        sign = "+"
+        flag="+"
+        res=[]
+        temp=0
         for i in range(len(s)):
             if s[i].isdigit():
-                operand = operand*10+int(s[i])
-            if s[i] in ['+', '-', '*', '/'] or i ==len(s)-1:
-                if sign =="+":
-                    stack.append(+operand)
-                elif sign =="-":
-                    stack.append(-operand)
-                elif sign == "*":
-                    val = stack.pop()
-                    stack.append(val*operand)
+                temp=temp*10+int(s[i])
+            if s[i] in ["+","-","*","/"] or i ==len(s)-1:
+                if flag=="+":
+                    res.append(temp)
+                elif flag=="-":
+                    res.append(-temp)
+                elif flag=="*":
+                    val = res.pop()
+                    res.append(val*temp)
                 else:
-                    val = stack.pop()
-                    stack.append(int(val/operand))
-                sign = s[i]
-                operand = 0
-      
-        return sum(stack)         
+                    val = res.pop()
+                    res.append(int(val/temp))
+                flag=s[i]    
+                temp=0     
+                    
+                    
+                
             
-            
+        return sum(res)           
+                    
+                    
+                    
+                

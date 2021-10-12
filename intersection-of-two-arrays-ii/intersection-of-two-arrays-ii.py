e243e1fbaf33278275ec24b5da72ee617015dcd1
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-12 11:13:08 -0400

    Time: 83 ms (17.82%), Space: 14.6 MB (10.38%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,23 +1,20 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         dic1={}
-        dic2={}
-        
         for i in nums1:
             if i not in dic1:
                 dic1[i]=[i]
             else:
                 dic1[i].append(i)
+        dic2={}
         for i in nums2:
             if i not in dic2:
                 dic2[i]=[i]
             else:
                 dic2[i].append(i)
-        
         res=[]
         for i in dic1:
             if i in dic2:
                 res+=min(dic1[i],dic2[i])
         return res        
-            
-                
+        

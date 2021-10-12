6d666d6d27b8ed5bbc23e9c020d83f0c2d15265a
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-12 12:11:34 -0400

    Time: 94 ms (33.94%), Space: 18.4 MB (10.45%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,14 +7,19 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        return self.helper(root,p,q)
+    
+    def helper(self,root,p,q):
+        if not root:
+            return 
+        if p==root or q==root:
+            return root
+        if root.val <p.val and root.val>q.val:
+            return root
+        if root.val >p.val and root.val<q.val:
+            return root
+        if root.val<p.val and root.val<q.val:
+            return self.helper(root.right,p,q)
+        if root.val>p.val and root.val>q.val:
+            return self.helper(root.left,p,q)
         
-        if p.val==root.val or q.val==root.val:
-            return root
-        if p.val<root.val and q.val>root.val:
-            return root
-        if p.val >root.val and q.val<root.val:
-            return root
-        if p.val <root.val and q.val<root.val:
-            return self.lowestCommonAncestor(root.left,p,q)
-        if p.val >root.val and q.val>root.val:
-            return self.lowestCommonAncestor(root.right,p,q)  

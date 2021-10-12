df3a2250d7e1d3e221e737bfb87cccb284098978
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-12 14:30:34 -0400

    Time: 43 ms (29.09%), Space: 14.3 MB (45.58%) - LeetHub

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -3,16 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p0=cur=0
-        p2 = len(nums)-1
+        zero=0
+        one=0
+        two=len(nums)-1
         
-        while cur<=p2:
-            if nums[cur]==0:
-                nums[cur],nums[p0]=nums[p0],nums[cur]
-                cur+=1
-                p0+=1
-            elif nums[cur]==2:
-                nums[cur],nums[p2]=nums[p2],nums[cur]
-                p2-=1
-            else:
-                cur+=1
+        while one<=two:
+            if nums[one]==0:
+                nums[zero],nums[one]=nums[one],nums[zero]
+                zero+=1
+                one+=1
+            elif nums[one]==1:
+                one+=1
+            elif nums[one]==2:
+                nums[one],nums[two]=nums[two],nums[one]
+                two-=1
+          
+                

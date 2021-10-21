8ec2353e99335049d5708c255eb578d8a13bc180
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-21 09:53:47 -0400

    Time: 82 ms (15.43%), Space: 14.5 MB (44.16%) - LeetHub

--- minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,23 +1,20 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp = [float("inf") for i in range(max(days)+1)]
+        dp=[float("inf")for i in range(max(days)+1)]
         dp[0]=0
-
-        price ={1:costs[0],7:costs[1],30:costs[2]}
+        prices={1:costs[0],7:costs[1],30:costs[2]}
         
         for i in range(1,len(dp)):
             if i not in days:
                 dp[i]=dp[i-1]
                 continue
-            
             for j in [1,7,30]:
-                if i>=j:
-                    dp[i]=min(dp[i],dp[i-j]+price[j])
+                if i >=j:
+                    dp[i]=min(dp[i-j]+prices[j],dp[i])
                 else:
-                    dp[i]=min(dp[i],dp[0]+price[j])
-           
-        return dp[-1]             
+                    dp[i]=min(dp[i],prices[j]+dp[0])
+        print(dp)            
+        return dp[-1]            
                 
                 
-        
-        
+            

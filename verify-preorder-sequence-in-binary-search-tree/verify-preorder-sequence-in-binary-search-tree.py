e46e59b0e634f38336da8c7d3fdc1bc179977c50
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-24 17:44:28 -0400

    Time: 432 ms (5.70%), Space: 15.1 MB (89.36%) - LeetHub

--- verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
+++ verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.py
@@ -1,11 +1,12 @@
 class Solution:
     def verifyPreorder(self, preorder: List[int]) -> bool:
-        stack =[]
-        node = None
-        for val in preorder:
-            while stack and val>stack[-1]:
-                node = stack.pop()
-            if node !=None and node>val:
+        stack=[]
+        check=None
+        for i in preorder:
+            while stack and i>=stack[-1]:
+                check=stack.pop()
+            if check!=None and check>i:
                 return False
-            stack.append(val)
+            stack.append(i)
         return True    
+            

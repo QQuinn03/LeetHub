76b0c504b530cba99a7a832270daab9b870fc972
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-25 13:00:10 -0400

    Time: 79 ms (7.83%), Space: 14.2 MB (52.46%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -3,30 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #1 2 6 4 2
-        #nums[i]<nums[i+1] 2
-        #[6 4 2] cloest to 2   4
-        #14226
-        
-        idx = len(nums)-2
-        
-        while idx!=-1:
-            if nums[idx]>=nums[idx+1]:
-                idx-=1
-            else:
-                break
-        if idx ==-1:
-           
+        idx=len(nums)-2
+        while idx>=0 and nums[idx]>=nums[idx+1]:
+            idx-=1
+        if idx==-1:
+            print(idx)
             return nums.sort()
-        print(idx)
-        change = idx+1
+        change=idx+1
+        
         while change<len(nums) and nums[change]>nums[idx]:
             change+=1
-            
         change-=1
-        print(change)
+        
         nums[idx],nums[change]=nums[change],nums[idx]
         nums[idx+1:]=sorted(nums[idx+1:])
+        print(nums)
         return nums
         
         

6dbe129d5fd1be447137990091c7b332cbc6fb5b
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-10-26 11:32:15 -0400

    Time: 360 ms (51.17%), Space: 13.7 MB (53.91%) - LeetHub

--- max-consecutive-ones-ii/max-consecutive-ones-ii.py
+++ max-consecutive-ones-ii/max-consecutive-ones-ii.py
@@ -4,29 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left = 0
-        right = 0
-        one = 0
-        res = 0
-        temp = 0
-        start=0
+        one=0
+        left=0
+        right=0
+        res=0
+        
         while right<len(nums):
-            if nums[right] ==1:
+            if nums[right]==1:
                 pass
             if nums[right]==0:
                 one+=1
-                
             while one>1:
-                start=nums[left]
-                if start==0:
+                if nums[left]==0:
                     one-=1
                 left+=1
-            res = max(res,right-left+1)    
-            right+=1     
-                
-                
-                
-                
-        return res    
-                    
-                
+            res=max(res,right-left+1)
+            right+=1
+        return res     

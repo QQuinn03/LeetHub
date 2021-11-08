4c02208a49a8943925fcd94bc3acd473a39816e5
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-08 11:07:22 -0500

    Time: 572 ms (76.67%), Space: 15 MB (61.33%) - LeetHub

---
+++ as-far-from-land-as-possible/as-far-from-land-as-possible.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxDistance(self, grid: List[List[int]]) -> int:
+      
+        n = len(grid)
+        que=deque([])
+        
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j]==1:
+                    que.append((i,j,0))
+        distance=0
+        visited=set()
+        
+        while que:
+            for i in range(len(que)):
+                x,y,d=que.popleft()
+          
+              
+                distance=max(distance,d)
+                
+                
+                for i in [(0,1),(0,-1),(1,0),(-1,0)]:
+                    new_x=i[0]+x
+                    new_y=i[1]+y
+                    if new_x>=0 and new_x<n and new_y>=0 and new_y<n:
+                        
+                            if grid[new_x][new_y]==0:
+                                grid[new_x][new_y]=1
+                                que.append((new_x,new_y,d+1))
+        return distance or -1                
+            
+        

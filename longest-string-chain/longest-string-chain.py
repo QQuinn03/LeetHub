1fab618c818f5a2f7bd7534ea4ce4d16b56ef8c1
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-08 21:13:42 -0500

    Time: 112 ms (98.55%), Space: 14.9 MB (42.33%) - LeetHub

--- longest-string-chain/longest-string-chain.py
+++ longest-string-chain/longest-string-chain.py
@@ -1,19 +1,14 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        graph=defaultdict(set)
-        size=len(words)
-        words.sort(key=lambda x:len(x))
+        words=sorted(words,key=lambda x:len(x))
+        dic = {word:1 for word in words}
         
-        for i in range(size):
-            word=words[i]
-            for j in range(len(word)):
-                graph[word[:j]+word[j+1:]].add(i)
-        print(graph)        
-        
-        dis=[1]*size
-        ans=1
-        for i in range(size):
-            for nei in graph[words[i]]:
-                dis[nei]=max(dis[nei],dis[i]+1)
-                ans=max(ans,dis[nei])
-        return ans         
+        for word in words:
+            for i in range(len(word)):
+                cur = word[:i]+word[i+1:]
+                if cur in dic:
+                    dic[word]=max(dic[word],dic[cur]+1)
+        return max(dic.values())             
+            
+            
+            

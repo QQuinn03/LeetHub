51b75d2dbe2368cf82cb27d79711f3e38fd8296d
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-10 20:51:00 -0500

    Time: 6120 ms (63.64%), Space: 14.3 MB (73.41%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -2,32 +2,32 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         row = len(board)
         col = len(board[0])
-        required = 0
-        visited=[[0 for i in range(col)]for j in range(row)]
+        required=0
+        visited=[[0 for i in range(col)] for j in range(row)]
+        
         for i in range(row):
             for j in range(col):
                 if self.helper(board,i,j,required,word,row,col,visited)==True:
-                        return True
-        return False
+                    return True
+        return False        
     
     def helper(self,board,i,j,required,word,row,col,visited):
         if required==len(word):
             return True
-        if i <0 or j<0 or i>=row or j>=col or board[i][j]!=word[required]:
+        if i<0 or j<0 or i>=row or j>=col or board[i][j]!=word[required]:
             return False
         if visited[i][j]==True:
             return False
-        required+=1
         visited[i][j]=True
+        required+=1
         
-        if self.helper(board,i-1,j,required,word,row,col,visited)==True:
-            return True
         if self.helper(board,i+1,j,required,word,row,col,visited)==True:
+            return True
+        if self.helper(board,i-1,j,required,word,row,col,visited)==True:
             return True
         if self.helper(board,i,j-1,required,word,row,col,visited)==True:
             return True
         if self.helper(board,i,j+1,required,word,row,col,visited)==True:
             return True
         visited[i][j]=False
-        
                 

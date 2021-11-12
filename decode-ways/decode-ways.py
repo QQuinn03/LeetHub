c7e6c7b0f41420458a251d849e19b0407aac8561
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-11 21:57:43 -0500

    Time: 24 ms (48.85%), Space: 13.8 MB (11.94%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -4,17 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dp = [0 for i in range(len(s)+1)]
+        dp=[0 for i in range(len(s)+1)]
         dp[0]=1
-        if s[0]=="0":
-            dp[1]=0
-        else:
+        if s[0]!="0":
             dp[1]=1
+        else:
+            dp[1]=0
         
-        for i in range(2,len(s)+1):
-            if 1<=int(s[i-1:i])<=9:
+        for i in range(2,len(dp)):
+            if 0<int(s[i-1:i])<=9:
+              
                 dp[i]+=dp[i-1]
             if 10<=int(s[i-2:i])<=26:
                 dp[i]+=dp[i-2]
-     
-        return dp[len(s)]        
+        print(dp)
+        return dp[-1]        
+      

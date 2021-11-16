2e345adf9f8e34b3352d2e8fa5fac237c864b521
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-16 12:04:56 -0500

    Time: 40 ms (81.41%), Space: 14.4 MB (20.60%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -7,17 +7,11 @@ class Solution:
         while idx>=0 and nums[idx]>=nums[idx+1]:
             idx-=1
         if idx==-1:
-            print(idx)
             return nums.sort()
-        change=idx+1
-        
+        change = idx+1
         while change<len(nums) and nums[change]>nums[idx]:
             change+=1
         change-=1
-        
         nums[idx],nums[change]=nums[change],nums[idx]
         nums[idx+1:]=sorted(nums[idx+1:])
-        print(nums)
         return nums
-        
-        

342a45ff4256dcdbc9509a8bfafbc99c5bfe5fbe
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-16 10:52:41 -0500

    Time: 84 ms (77.07%), Space: 15.4 MB (83.66%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,39 +1,38 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        res = [-1,-1]
-        res[0]=self.helperL(nums,target)
-        res[-1]=self.helperR(nums,target)
+        
+        res=[-1,-1]
+        res[0]=self.left(nums,target)
+        res[1]=self.right(nums,target)
+        
         return res
-    def helperL(self,nums,target):
-        if target not in nums:
-            return -1
-        l = 0 
-        r = len(nums)-1
-        
-        while l<=r:
-            mid = l+(r-l)//2
-            if nums[mid]<target:
-                l = mid +1
-            else:
-                r = mid -1
-        return l
     
-    def helperR(self,nums,target):
+    def left(self,nums,target):
         if target not in nums:
             return -1
-        l = 0 
-        r = len(nums)-1
+        l=0
+        r=len(nums)-1
         
         while l<=r:
-            mid = l+(r-l)//2
+            mid=l+(r-l)//2
+            if nums[mid]>=target:
+                r = mid-1
+            elif nums[mid]<target:
+                l = mid+1
+        return l 
+    
+    def right(self,nums,target):
+        if target not in nums:
+            return -1
+        l=0
+        r=len(nums)-1
+        
+        while l<=r:
+            mid=l+(r-l)//2
+            if nums[mid]>target:
+                r = mid-1
             if nums[mid]<=target:
-                l = mid +1
-            else:
-                r = mid -1
-        if l ==0:
-            return 0
-        return l-1
-        
-        
-        
+                l=mid+1
+                
+        return l-1  
         

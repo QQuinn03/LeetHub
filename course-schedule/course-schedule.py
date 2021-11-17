3ab608bb1e28111138b8b7718dacc53e65f65b9b
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-16 21:17:06 -0500

    Time: 88 ms (39.66%), Space: 15.8 MB (59.60%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -6,29 +6,30 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        todo = {i:set()for i in range(numCourses)}
-        pre= {i:set()for i in range(numCourses)}
+        course={i:set() for i in range(numCourses)}
+        todo={i:set() for i in range(numCourses)}
         
         for i in prerequisites:
-            course=i[0]
-            course_pre=i[1]
-            todo[course].add(course_pre)
-            pre[course_pre].add(course)
-            
-        que= deque([])
-        for i in todo:
-            if len(todo[i])==0:
+            course[i[0]].add(i[1])
+            todo[i[1]].add(i[0])
+        que = deque([])
+        for i in course:
+            if len(course[i])==0:
                 que.append(i)
-        visited=set()
-        
+        seen=set()
         while que:
-            node = que.popleft()
-            visited.add(node)
-            for neighbor in pre[node]:
-                todo[neighbor].remove(node)
-                if len(todo[neighbor])==0:
-                    que.append(neighbor)
-        if len(visited)==numCourses:
-            return True
-        return False
+            node =que.popleft()
+           
+            seen.add(node)
+            for nei in todo[node]:
+                course[nei].remove(node)
+                if len(course[nei])==0:
+                    que.append(nei)
+        if len(seen)!=numCourses:
+            return False
+        return True
+            
+        
+       
+    
             

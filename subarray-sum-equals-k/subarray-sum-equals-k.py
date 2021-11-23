cf702c0b910ae1d23bb44f6e72cdc76266442661
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-23 10:18:52 -0500

    Time: 244 ms (94.16%), Space: 16.8 MB (25.39%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.py
+++ subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,17 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         dic={0:1}
-        
         res=0
         total=0
-        for i in nums:
-            total+=i
+        for i in range(len(nums)):
+            total+=nums[i]
             if total-k in dic:
                 res+=dic[total-k]
             if total in dic:
                 dic[total]+=1
-            if total not in dic:
+            else:
                 dic[total]=1
         return res        
+                
             
+        
+        
             

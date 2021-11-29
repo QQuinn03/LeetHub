1da65a4c36264a416a6c123aeccbaabfc4b100a1
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-29 12:15:36 -0500

    Time: 116 ms (29.22%), Space: 15 MB (91.06%) - LeetHub

--- remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -3,19 +3,14 @@ class Solution:
         dic={}
         for i in range(len(s)):
             dic[s[i]]=i
+        
         stack=[]
         seen=set()
-        
         for idx,char in enumerate(s):
-      
             while stack and stack[-1]==char:
                 stack.pop()
                 break
             else:    
                 stack.append(char)
-        return "".join(stack)      
+        return "".join(stack) 
             
-            
-        
-         
-        

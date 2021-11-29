3a9401f36d0a66ee2cc6c943fe0cc520b7f21f47
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-29 14:56:07 -0500

    Time: 9392 ms (5.02%), Space: 14.7 MB (77.92%) - LeetHub

---
+++ expression-add-operators/expression-add-operators.py
@@ -0,0 +1,34 @@
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        arr=list(num)
+   
+        path=""
+        res=[]
+
+        idx=0
+        self.helper(arr,idx,path,res,target)
+        return res
+    
+    def helper(self,arr,idx,path,res,target):
+        if idx>=len(arr):
+            return 
+        if idx==len(arr)-1:
+            
+            path=path+arr[idx]
+            if eval(path)==target:
+          
+                res.append(path)
+                return 
+  
+        self.helper(arr,idx+1,path+arr[idx]+"+",res,target)
+        self.helper(arr,idx+1,path+arr[idx]+"*",res,target)
+        self.helper(arr,idx+1,path+arr[idx]+"-",res,target)
+        if (path and path[-1] not in ["-","+","*"] and arr[idx]=="0") or arr[idx]!="0":
+        
+            self.helper(arr,idx+1,path+arr[idx],res,target)
+            
+        
+        
+                
+            
+        

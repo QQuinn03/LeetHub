06e63907a8dfee8f6fff804994768b3b139ce9b1
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-11-29 10:09:57 -0500

    Time: 28 ms (98.51%), Space: 14.5 MB (18.22%) - LeetHub

--- group-shifted-strings/group-shifted-strings.py
+++ group-shifted-strings/group-shifted-strings.py
@@ -1,14 +1,19 @@
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        dic = {}
+        for i in strings:
+            diff = ()
+            for j in range(1,len(i)):
+                key = (ord(i[j])-ord(i[j-1]))%26
+                diff+=(key,)
+            print(diff)    
+            if diff not in dic:
+                dic[diff]=[i]
+            else:
+                dic[diff].append(i)
+        res=[]        
+        for val in dic.values():
+            res.append(val)
+        return res    
         
-        hashmap = {}
-        for s in strings:
-            key = ()
-            for i in range(len(s) - 1):
-                circular_difference =ord(s[i+1]) - ord(s[i])
-                key+=(circular_difference % 26,)
-                
-            hashmap[key] = hashmap.get(key, []) + [s]
-            # print(hashmap)
-        return list(hashmap.values())
         

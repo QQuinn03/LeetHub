0693e39c2da78d48940209d14d783831fd55b41c
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-12-02 12:20:31 -0500

    Time: 92 ms (35.54%), Space: 15.5 MB (54.89%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -13,10 +13,10 @@ class Solution:
         l=0
         r=len(nums)-1
         
-        while l<=r:
+        while l<r:
             mid=l+(r-l)//2
             if nums[mid]>=target:
-                r = mid-1
+                r = mid
             elif nums[mid]<target:
                 l = mid+1
         return l 

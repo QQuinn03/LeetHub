dc8fc6e0bfa9883134207d413bc3b9aacd78f981
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-12-04 17:42:41 -0500

    Time: 100 ms (82.30%), Space: 14.7 MB (89.24%) - LeetHub

--- minimum-window-substring/minimum-window-substring.py
+++ minimum-window-substring/minimum-window-substring.py
@@ -1,39 +1,49 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         required={}
-        count=0
-        for i in t:
-            if i not in required:
-                required[i]=1
+        for char in t:
+            if char not in required:
+                required[char]=1
             else:
-                required[i]+=1
-        required_size=len(required)        
-        res=(float("inf"),0,0)
-        dic={}
-        check=0
-        right=0
+                required[char]+=1
+        required_length=len(required)
+        
+        check={}
         left=0
+        right=0
+        res=(float("inf"),left,right)
+        count=0
         
         while right<len(s):
-            if s[right] not in dic:
-                dic[s[right]]=1
+            if s[right] not in check:
+                check[s[right]]=1
             else:
-                dic[s[right]]+=1
-            if s[right]in required and dic[s[right]]==required[s[right]]:
+                check[s[right]]+=1
+                
+            if s[right] in required and check[s[right]]==required[s[right]]:
                 count+=1
-            while count== required_size and left<=right:
-                char = s[left]
+                
+            while count== required_length and left<=right:
+                char=s[left]
+                
                 if right-left+1<res[0]:
                     res=(right-left+1,left,right)
-                dic[char]-=1
-                if char in required and dic[char]<required[char]:
+                    
+                check[char]-=1
+                if char in required and check[char]<required[char]:
                     count-=1
-                left+=1    
+                left+=1
+                
             right+=1
-        left=res[1]
-        right=res[2]
+            
         if res[0]==float("inf"):
             return ""
+        left=res[1]
+        right=res[2]
         return s[left:right+1]
+                
+        
+                
+        
                 
             

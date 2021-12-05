8233d5863fc04e3027bdafc5c5f2869df7b37b25
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-12-05 11:35:08 -0500

    Time: 68 ms (93.32%), Space: 15.4 MB (60.52%) - LeetHub

--- basic-calculator/basic-calculator.py
+++ basic-calculator/basic-calculator.py
@@ -1,38 +1,35 @@
-from collections import deque
 class Solution:
     def calculate(self, s: str) -> int:
         stack=[]
-        sign=1
+        operand=0
         res=0
-        operand = 0
+        sign=1
         
         for i in s:
             if i.isdigit():
                 operand=operand*10+int(i)
+                continue
+            if i =="+":
+                res=res+operand*sign
+                sign=1
+                operand=0
             elif i =="-":
-                res+=operand*sign
+                res=res+operand*sign
                 sign=-1
                 operand=0
-            elif i=="+":
-                res+=operand*sign
-                sign=1
-                operand =0
             elif i=="(":
                 stack.append(res)
                 stack.append(sign)
                 res=0
                 sign=1
-                operand=0
             elif i==")":
                 res+=operand*sign
                 res*=stack.pop()
                 res+=stack.pop()
                 operand=0
-
+                sign=1
         return res+operand*sign        
+                
+                
+                
         
-                    
-  
-            
-        
-     

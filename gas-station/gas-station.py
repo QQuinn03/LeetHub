bee8040f49906210473268d6f9e5ba13a32f2212
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-12-12 12:28:48 -0500

    Time: 392 ms (81.36%), Space: 19.8 MB (56.11%) - LeetHub

--- gas-station/gas-station.py
+++ gas-station/gas-station.py
@@ -1,15 +1,20 @@
-class Solution:
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        index=0
         cur_gas=0
         max_gas=0
-        res=0
         
         for i in range(len(gas)):
             cur_gas+=gas[i]-cost[i]
             max_gas+=gas[i]-cost[i]
             if cur_gas<0:
+                index=i+1
                 cur_gas=0
-                res=i+1
         if max_gas<0:
             return -1
-        return res
+        return index

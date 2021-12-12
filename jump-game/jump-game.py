de0904ca3e87667a5a4d3cb09d59d38971605119
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-12-12 11:39:32 -0500

    Time: 420 ms (57.39%), Space: 14.2 MB (97.41%) - LeetHub

--- jump-game/jump-game.py
+++ jump-game/jump-game.py
@@ -1,14 +1,12 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        dp=[False for i in range(len(nums))]
-        dp[0]=True
-        cur_max=0+nums[0]
-        
-        for i in range(1,len(nums)):
-            if i >cur_max:
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        cur_max=0
+        for i in range(len(nums)):
+            if cur_max<i:
                 return False
-            dp[i]=True
             cur_max=max(cur_max,nums[i]+i)
-            print(cur_max)
-        return dp[-1]    
-        
+        return True    

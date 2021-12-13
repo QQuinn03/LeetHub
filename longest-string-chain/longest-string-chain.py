c799d6c34e7272a78a7461bee8fee322dfbe18f0
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-12-13 10:12:08 -0500

    Time: 116 ms (71.39%), Space: 13.9 MB (60.29%) - LeetHub

--- longest-string-chain/longest-string-chain.py
+++ longest-string-chain/longest-string-chain.py
@@ -1,14 +1,17 @@
-class Solution:
-    def longestStrChain(self, words: List[str]) -> int:
-        words=sorted(words,key=lambda x:len(x))
-        dic = {word:1 for word in words}
+class Solution(object):
+    def longestStrChain(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        words.sort(key=lambda x:len(x))
+        print(words)
+        dp={word:1 for word in words}
         
         for word in words:
-            for i in range(len(word)):
-                cur = word[:i]+word[i+1:]
-                if cur in dic:
-                    dic[word]=max(dic[word],dic[cur]+1)
-        return max(dic.values())             
-            
-            
-            
+            for j in range(len(word)):
+                temp=word[:j]+word[j+1:]
+                if temp in dp:
+                    dp[word]=max(dp[word],dp[temp]+1)
+        return max(dp.values())          
+                

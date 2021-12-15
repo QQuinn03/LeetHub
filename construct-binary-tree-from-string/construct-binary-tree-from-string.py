2acadfd366e709ad8d4968835b0c43338ece5fdc
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-12-15 12:34:19 -0500

    Time: 164 ms (37.88%), Space: 14.8 MB (46.97%) - LeetHub

--- construct-binary-tree-from-string/construct-binary-tree-from-string.py
+++ construct-binary-tree-from-string/construct-binary-tree-from-string.py
@@ -1,25 +1,36 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def str2tree(self, s: str) -> Optional[TreeNode]:
+class Solution(object):
+    def str2tree(self, s):
+        """
+        :type s: str
+        :rtype: TreeNode
+        """
+        if not s:
+            return None
+        
         stack=[]
-        num=""
+        flag=1
+        temp=""
         for i in s:
-            if i.isdigit() or i=="-":
-                num +=i
+            if i=="-" or i.isdigit():
+                temp+=i
+                
             elif i=="(":
-                if num:
-                    node=TreeNode(int(num))
+                if temp:
+                  
+                    node=TreeNode(int(temp))
                     stack.append(node)
-                    num=""
-            else:
-                if num:
-                    node =TreeNode(int(num))
-                    num=""
+                    temp=""
+           
+            elif i==")":
+                if temp:
+                    node=TreeNode(int(temp))
+                    temp=""
                     if stack[-1].left==None:
                         stack[-1].left=node
                     else:
@@ -35,7 +46,8 @@ class Solution:
         if stack:
             return stack[-1]
         else:
-            return TreeNode(int(s))
-                    
-                
+            return TreeNode(int(s)) 
+                        
+
+         
                 

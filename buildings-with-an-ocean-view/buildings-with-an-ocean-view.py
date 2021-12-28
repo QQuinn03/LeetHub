2ae65b8adc5131b6e2684deb0130c954d90c813a
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-12-28 10:40:31 -0500

    Time: 760 ms (37.64%), Space: 31.4 MB (80.47%) - LeetHub

--- buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,7 +1,6 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         stack=[]
-        
         for i,val in enumerate(heights):
             while stack and heights[stack[-1]]<=val:
                 stack.pop()

7116ea713591b8842471f7d6c15a1db08a798252
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2021-12-29 11:32:42 -0500

    Time: 126 ms (17.39%), Space: 14.3 MB (56.71%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -6,11 +6,11 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        que=[]
+        res =[]
+      
         for i in nums:
-            if len(que)==k:
-                heapq.heappushpop(que,i)
+            if len(res)<k:
+                heapq.heappush(res,i)
             else:
-                heapq.heappush(que,i)
-             
-        return que[0]         
+                heapq.heappushpop(res,i)
+        return res[0]        

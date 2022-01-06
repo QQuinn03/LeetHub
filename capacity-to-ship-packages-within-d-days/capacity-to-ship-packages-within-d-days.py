51ce21d721f0f524a07ed24266fb5fca473fcaa5
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2022-01-06 14:22:01 -0500

    Time: 772 ms (24.03%), Space: 17 MB (49.14%) - LeetHub

--- capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,27 +1,22 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        low=max(weights)
+        low = max(weights)
         high=sum(weights)
-        res=0
-        
+        day=1
+       
         while low<high:
+            day=1
             mid=low+(high-low)//2
             total=0
-            day=1
-            
             for weight in weights:
                 total+=weight
                 if total>mid:
-                    total=weight
                     day+=1
-                    
+                    total=weight
             if day>days:
                 low=mid+1
             else:
                 high=mid
-        return low        
-                    
-                    
+        return low       
                 
-            
-        
+                

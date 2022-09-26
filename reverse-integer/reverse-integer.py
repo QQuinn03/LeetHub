91a2a4b4eb970e8c8596a78b8825470db4f3be4d
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2022-09-26 12:50:58 -0400

    Time: 40 ms (27.94%), Space: 13.2 MB (86.31%) - LeetHub

--- reverse-integer/reverse-integer.py
+++ reverse-integer/reverse-integer.py
@@ -1,19 +1,28 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        res = 0
-        flag =1
-        min_num = -2**31
-        max_num = 2**31-1
-        if x <0:
-            flag = -1
-            x= abs(x)
-        while x!=0 :
-            temp = x%10
-            res = res*10+temp
-            x = x //10
-        
-        if flag *res>max_num or flag*res<min_num:
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x==0:
             return 0
-        if flag*res<0:
-            return max(min_num,flag*res)
-        return min(max_num,flag*res)
+        
+        res=0
+        count = 0
+        flag=1
+        
+        if x<0:
+            flag=-1
+            x*=-1
+            
+        while x!=0:
+            digit = x%10
+            x=x//10
+            res=res*10+digit
+            
+        if flag==-1:
+            res*=-1
+        if res>2**31-1 or res<-2**31-1:
+            return 0
+        return res
+            

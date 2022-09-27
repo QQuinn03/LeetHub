315288cde8f3ed3a7692e43ee7d88953a378af9d
Qiong Qin <46905011+QQuinn03@users.noreply.github.com>
2022-09-27 12:52:06 -0400

    Time: 28 ms (52.38%), Space: 13.5 MB (65.85%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,36 +1,33 @@
 class Solution(object):
-    
     def __init__(self):
-        
         self.dic = {"2":["a","b","c"], 
-               "3":["d","e","f"],
-               "4":["g","h","i"],
-               "5":["j","k","l"],
-               "6":["m","n","o"],
-               "7":["p","q","r","s"], 
-               "8":["t","u","v"],
-               "9":["w","x","y","z"]} 
-        
+                   "3":["d","e","f"],
+                   "4":["g","h","i"],
+                   "5":["j","k","l"],
+                   "6":["m","n","o"],
+                   "7":["p","q","r","s"], 
+                   "8":["t","u","v"],
+                   "9":["w","x","y","z"]} 
+    
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
-        if len(digits)==0:
-            return digits
-        res = []
-        self.helper(digits,"",res,len(digits))
+        if digits=="":
+            return []
+        res=[]
+        size=len(digits)
+        combine=""
+        self.helper(digits,res,size,combine)
         return res
     
-    def helper(self,digits,path,res,size):
-        if len(path)==size:
-            res.append(path)
-            return
+    def helper(self,digits,res,size,combine):
+        if len(combine)==size:
+            res.append(combine)
+            return 
         for i in range(len(digits)):
-            for c in self.dic[digits[i]]:
-                self.helper(digits[i+1:],path+c,res,size)
-            
-            
-            
-  
- 
+            for char in self.dic[digits[i]]:
+                self.helper(digits[i+1:],res,size,combine+char)
+        
+        
